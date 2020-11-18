0744b8a5f528c7aa12ee4b2b87f35407e2dfc497
riskykurniawan945 <73045161+riskykurniawan945@users.noreply.github.com>
2020-11-18 20:10:33 +0800

    Update POSTEST3(Angka (1-9)).py

--- POSTEST3(Angka (1-9)).py
+++ POSTEST3(Angka (1-9)).py
@@ -1,25 +1,28 @@
 # Angka (1-9)
-# Karena Program akan mengoutput angka nol jika kelipatan 9
-# Bisa diakali dengan print(9)
 
+# CARA 1
 
 angka = int(input("Masukkan Angka =    "))
-
 for i in range(angka):
-    if ((i+1) % 9 == 0):
-        print(9)
-    else:
-        print ((i+1) % 9)
+    print(9) if ((i+1) % 9 == 0) else print ((i+1) % 9)
 
 
-
-
-# # DENGAN INPUT MENGGUNAKAN NIM
-# # NIM = 2009106050
-
-# nim = int(input("Masukkan NIM =    "))
-# angka = int(input("Ingin Menghitung berapa kali =    "))
+# CARA 2
+# Cara dibawah kurang efektif karena menambah variabel hanya untuk print output
+# j = 1
 # for i in range(angka):
-#     print (nim + (i)) # ini jika tidak mau angka berulang di 1-10
+#     print(j)
+#     j += 1
+#     if (j == 10):
+#         j -= 9
 
-#     # print (nim + (i % 10)) # ini akan selalu mengulang 1-10
+
+# CARA 3
+# a = 1
+# b = 1
+# while(a <= angka):
+#     print(b)
+#     b += 1
+#     if (b == 10):
+#         b -= 9
+#     a += 1
